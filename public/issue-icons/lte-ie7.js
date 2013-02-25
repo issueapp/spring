bc8a0554f836d5a4119cdd3ef0a10d1930c13162
Taylor Luk <taylor.luk@idealian.net>
2013-02-25 16:31:31 +1100

    Make spring a design/prototype tool
     - /web is the website
     - /app is the responsive app
     - /docs is the framework documentation
    
     - (imported a sample WIP issue, needs a lot more work)

---
+++ public/issue-icons/lte-ie7.js
@@ -0,0 +1,62 @@
+/* Use this script if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'issue-icons\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-export' : '&#xe000;',
+			'icon-heart' : '&#xe001;',
+			'icon-earth' : '&#xe002;',
+			'icon-network' : '&#xe003;',
+			'icon-docs' : '&#xe004;',
+			'icon-twitter' : '&#xe007;',
+			'icon-facebook' : '&#xe008;',
+			'icon-googleplus' : '&#xe005;',
+			'icon-pinterest' : '&#xe009;',
+			'icon-camera' : '&#xe006;',
+			'icon-link' : '&#xe00a;',
+			'icon-comment' : '&#xe00b;',
+			'icon-cog' : '&#xe00c;',
+			'icon-arrow-left' : '&#xe00d;',
+			'icon-arrow-right' : '&#xe00e;',
+			'icon-arrow-left-2' : '&#xe00f;',
+			'icon-arrow-right-2' : '&#xe010;',
+			'icon-pictures' : '&#xe011;',
+			'icon-video' : '&#xe012;',
+			'icon-music' : '&#xe013;',
+			'icon-list' : '&#xe014;',
+			'icon-plus' : '&#xe015;',
+			'icon-minus' : '&#xe016;',
+			'icon-read' : '&#xe017;',
+			'icon-shop' : '&#xe01b;',
+			'icon-book' : '&#xe01c;',
+			'icon-layout' : '&#xe01d;',
+			'icon-ccw' : '&#xe01e;',
+			'icon-info' : '&#xe01f;',
+			'icon-question' : '&#xe020;',
+			'icon-user' : '&#xe02a;',
+			'icon-tag' : '&#xe018;',
+			'icon-search' : '&#xe019;',
+			'icon-stackoverflow' : '&#xe01a;',
+			'icon-stream' : '&#xe021;',
+			'icon-play' : '&#xe022;',
+			'icon-checkmark' : '&#xe023;',
+			'icon-cross' : '&#xe024;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
---
+++ public/issue-icons/readme.txt
@@ -0,0 +1,7 @@
+Customize or download custom icon font
+
+- visit http://icomoon.io/app/
+- load "issue-icon.json" to restore the session
+- first tag will be the css class name (Ex. icon-play)
+
+
