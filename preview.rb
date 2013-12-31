7baa49164eee4c24b68e9677716c66d506d23971
gkwrh <gkwrh@hotmail.com>
2014-01-01 03:07:45 +1100

    updates issue template viewer, path: /viewer/guide/ or /viewer/guide/index
    Former-commit-id: 2265cd967e6d3263e19d27a0721da118af8435e4 [formerly 97e4ca67d638e438da0a61004ef2bca052615cd7 [formerly 3da6a5bcb6fbbb7b021185a441145a7f5b543de9]]
    Former-commit-id: 1f8ba1cbe9a2ba48f713c7c35e4aaba69f7b6635
    Former-commit-id: cd12c70f8fbd8376238067a02d49ffec31d74f9b

--- preview.rb
+++ preview.rb
@@ -2,11 +2,13 @@ require 'rdiscount'
 require 'hashie/mash'
 require 'sinatra/base'
 require 'sinatra/content_for'
+require 'json'
+require 'yaml'
 
 class IssuePreview < Sinatra::Base
   # helpers Serve::RenderHelpers
   helpers Sinatra::ContentFor
-  
+
   helpers do
     def render(*args)
       if args.first.is_a?(Hash) && args.first.keys.include?(:partial)
@@ -16,17 +18,23 @@ class IssuePreview < Sinatra::Base
       end
     end
   end
-  
-  get %r{/viewer/(?<issue>[^\/]+)/(?<page>[^\/]+)?(?:\/(?<subpage>[^\/]+))?} do
-    path = params.slice("issue", "page", "subpage").values.compact.join('/')
-    
-    erb :"issue/viewer.html", layout: :"/layouts/_docs.html", locals: { path: "/issues/#{path}", issue: current_issue }
+
+  get %r{/viewer/(?<issue>[^\/]+)/?(?<page>[^\/]+)?(?:\/(?<subpage>[^\/]+))?} do
+    if params["page"] =~ /(.+)\.json$/
+      path = File.expand_path("../issues/#{params["issue"]}/#{$1}.json", __FILE__)
+      open(path).read
+
+    else
+      path = params.slice("issue", "page", "subpage").values.compact.join('/')
+
+      erb :"issue/viewer.html", layout: :"/layouts/_docs.html", locals: { path: "/issues/#{path}", issue: current_issue }
+    end
   end
-  
+
   get '/issues/:issue' do
     redirect to("/issues/#{params[:issue]}/")
   end
-  
+
   get "/issues/:issue/" do
     erb :"issue/_cover.html", layout: :"issue/_layout.html", locals: { issue: current_issue}
   end
@@ -35,68 +43,71 @@ class IssuePreview < Sinatra::Base
   get %r{/issues/(?<issue>[^\/]+)/(?<page>[^\/]+)(?:\/(?<subpage>[^\/]+))?} do
     path = params.slice("issue", "page", "subpage").values.compact.join('/')
     file_path = File.expand_path("../issues/#{path}.md", __FILE__)
-    
+
     page = find_page(file_path)
     erb page_template(page), locals: { issue: current_issue, page: page  }, layout: :"issue/_layout.html"
   end
-  
+
   private
-  
+
   def current_issue
     issue = YAML.load_file(File.expand_path("../issues/#{params[:issue]}/issue.yaml", __FILE__))
     items = issue.fetch("items", []).map do |page|
       page["url"] = "#{page["handle"]}"
-      
+
       page
     end
-    
+
     issue.merge!(
       "image_url" => asset_path(issue["image_url"]),
       "items" => items
     )
-    
+
     Hashie::Mash.new(issue)
   end
-  
+
   # Find page data file and turn yaml in heading into page attribute, body as page content
   def find_page(path)
     source = open(path).read
-    
+
     if "1.9".respond_to? :encode
-      source = source.force_encoding('binary') 
+      source = source.force_encoding('binary')
     end
-    
-    ## Parse YAML 
+
+    ## Parse YAML
     meta, content = source.split(/---\n(.+?)---/nm)[1,2]
-    
+
     if meta
       attributes = YAML.load(meta)
     else
       attributes = {}
     end
-    
+
     attributes.merge!(
       "issue_url" => issue_url,
       "page_url" => "#{issue_url}/#{params[:page]}",
       "image_url" => asset_path(attributes["image_url"]),    # remove preview rendering
-      
+
       "content" => content && RDiscount.new(content).to_html,
       "published_at" => attributes["published_at"] || File.mtime(path),
       "layout" => attributes.fetch("layout", {})
     )
-    
+
     Hashie::Mash.new(attributes)
   end
+
   def asset_path(path)
     "#{issue_url}/#{path}"
   end
-  
+
   def issue_url
     "#{request.base_url}/#{params[:issue]}"
   end
-  
+
   def page_template(page)
-    if page.layout.type == "toc"
+    if page.layout.type == "cover"
+      :"issue/_cover.html"
+    elsif page.layout.type == "toc"
       :"issue/_toc.html"
     else
       :"issue/_page.html"
