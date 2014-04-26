fcfe5e410e5dbb76d7a94dd8df7d1441c1739760
Taylor Luk <taylor.luk@idealian.net>
2014-04-26 16:31:37 +1000

    Implemented page rendering helper
     - Make spec passing for audio_node, video_node and image_node.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
 # -*- encoding : utf-8 -*-
 require 'bundler'
-Bundler.setup
+Bundler.require :test
 ENV['RACK_ENV'] = ENV['RAILS_ENV'] = 'test'
 GEM_ROOT = Pathname.new(File.expand_path('../..', __FILE__))
-
-require 'rspec'
+# 
+# require 'rspec'
+require 'hashie'
+require 'local_issue'
+# require 'awesome_print'
