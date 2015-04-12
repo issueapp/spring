a6911f6528128ad682c9e27c5837f6166e761b0b
Mark Guo <markgk629@gmail.com>
2015-04-12 22:48:20 +1000

    energy: update ret question content & styles

---
+++ issues/energy/assets/custom.js
@@ -0,0 +1,37 @@
+var setupGeoLink = function() {
+  console.log('map is ready');
+};
+
+$(function() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCV5jQkaOQYZO9szxfYYyb06lAvihOjbMw&callback=setupGeoLink';
+  document.body.appendChild(script);
+
+
+  function resizeBgVideo() {
+    var video        = $('.cover-area.video video');
+    var imageWidth   = 1922;
+    var imageHeight  = 1080;
+    var headerHeight = video.parent().height();
+
+    if (video.length == 0)
+      return;
+
+    // video.playbackRate = 1.5
+
+    videoWidth  = video.parent().width();
+    videoHeight = videoWidth * imageHeight / imageWidth;
+
+    if (videoHeight <= headerHeight) {
+      videoHeight = headerHeight;
+      videoWidth  = headerHeight * imageWidth / imageHeight;
+    }
+
+    video[0].style.width  = parseInt(videoWidth) + 'px';
+    video[0].style.height = parseInt(videoHeight) + 'px';
+  }
+
+  resizeBgVideo();
+
+});
