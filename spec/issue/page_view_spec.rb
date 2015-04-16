17356492229c0cce02af1e4b7b366f8a5fd5f04e
khoa nguyen <huukhoanguyen@hotmail.com>
2015-04-16 23:04:16 +1000

    flesh out Issue::PageView spec
    Former-commit-id: 15a0c0faa9e570bc3d0b2cb5107b040c68a66985

--- spec/issue/page_view_spec.rb
+++ spec/issue/page_view_spec.rb
@@ -1,9 +1,9 @@
-require 'local_issue/page_view'
-#require 'issue/page_view'
+require 'local_issue/page'
+require 'issue/page_view'
 
 RSpec.describe Issue::PageView do
   let(:page) { LocalIssue::Page.new }
-  let(:view) { LocalIssue::PageView.new(page) }
+  let(:view) { Issue::PageView.new(page) }
 
   describe 'Rendering Helpers' do
     subject { view }
@@ -19,14 +19,16 @@ RSpec.describe Issue::PageView do
     it { should respond_to('category') }
 
     it { should respond_to('theme') }
-    it { should respond_to('author') }
     it { should respond_to('credits') }
+
+    it { should respond_to('show_author?') }
+    it { should respond_to('author') }
 
     it { should respond_to('has_parent?') }
     it { should respond_to('parent') }
-    it { should respond_to('parent_title') }
 
     it { should respond_to('layout') }
+    it { shoulde respond_to('column_break_count') }
 
     it { should respond_to('custom_layout?') }
     it { should respond_to('custom_layout_class') }
@@ -37,16 +39,34 @@ RSpec.describe Issue::PageView do
     it { should respond_to('cover?') }
     it { should respond_to('cover') }
     it { should respond_to('cover_html') }
-    #it { should respond_to('cover_layout_class') }
 
     it { should respond_to('product?') }
     it { should respond_to('products_html') }
   end
 
-  describe 'multicolumn'
-  describe 'products'
-  describe 'cover'
-  describe 'author'
+  describe 'author' do
+    it 'includes author name and icon'
+    it 'shows/hides author based on switch'
+    it 'shows author when there is an author'
+    it 'hides author on child pages'
+  end
+
+  describe 'multicolumn' do
+    it 'has 0 column break by default'
+    it 'has 1 column break for cover or product set'
+    it 'has 2 column breaks when three column cover takes up 2 column'
+  end
+
+  describe 'products' do
+    it 'detects products'
+    it 'renders product set html'
+  end
+
+  describe 'cover' do
+    it 'detects image cover'
+    it 'detects video cover'
+    it 'renders cover html'
+  end
 
   describe 'page elements' do
     describe 'video'
