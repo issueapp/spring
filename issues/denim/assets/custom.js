b8a12dbe2a067ae28d0bacc29a62a69df93c521c
Mark Guo <markgk629@gmail.com>
2015-04-29 14:30:19 +1000

    denim: brands to love page cards animations [wip]
    Former-commit-id: 67f3aa2cd184ce6ea2b83414e62db7fede4b7da7

--- issues/denim/assets/custom.js
+++ issues/denim/assets/custom.js
@@ -1,7 +1,7 @@
 $(function() {
   App.on("page:active", function(path) {
     if (path == "perfect-fit/1" || path == "perfect-fit-men/1" || path == "brands-to-love") {
-      $('article.page.current #flip-cards .flipper').eq(0).addClass('flip');
+      $('article.page.current').addClass('page-animation');
     }
   });
 });
