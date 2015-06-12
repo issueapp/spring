a7e3ee2ce388d198cdd60360a922a19cca8b08a7
Mark Guo <markgk629@gmail.com>
2015-06-12 15:34:27 +1000

    rebalance: update gut instinct page
    Former-commit-id: 4661af3aceae1b126ba5a2d25f2e0ca15885a5e0

--- issues/rebalance/assets/custom.js
+++ issues/rebalance/assets/custom.js
@@ -7,7 +7,12 @@ $(function() {
     }
   });
 
-  $(document).on('mouseenter touchstart', "a[href='#item8']", function() {
-    $(this).removeAttr('data-hint').removeClass('hint--always');
+  $(document).on('mouseenter touchstart', ".gut a", function() {
+    $("a[href='#item8']").removeAttr('data-hint').removeClass('hint--always');
+  });
+
+  $(document).on('click touchstart', ".gut a", function() {
+    $('.gut a').removeClass('active');
+    $(this).addClass('active');
   });
 });
